bbab260b21063a58472b6c89b7f7a05519b3681b
noah <qkrwldn0818@naver.com>
2024-03-19 17:26:27 +0900

    객체 지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDicountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +13,19 @@ import hello.core.order.OrderServiceIpml;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memeberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceIpml(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceIpml(memeberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memeberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDicountPolicy();
     }
 }
