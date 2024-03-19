bedd20fcea8fab44be3c331cf43fc90855e99380
noah <qkrwldn0818@naver.com>
2024-03-19 20:41:38 +0900

    스프링 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceIpml;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memeberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceIpml(memeberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memeberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDicountPolicy();
